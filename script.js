e02738055afcc746bacf2c86a7c3c24dc0e448ab
Jas802 <jas.spencer414@gmail.com>
2020-12-01 13:00:54 -0500

    added consts for all buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelector('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.queryCommandValue('[data-equals]');
+const deleteButton = document.queryCommandValue('[data-delete]'); 
+const allClearButton = document.queryCommandValue('[data-all-clear]'); 
+const previousOperationandTextElement = document.queryCommandValue('[data-previous-operand]'); 
+const currentOperationandTextElement = document.queryCommandValue('[data-current-operand]');
