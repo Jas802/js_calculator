13b756296d90886bf4923e173af12c3ee89512d2
Jas802 <jas.spencer414@gmail.com>
2020-12-02 13:27:23 -0500

    started Calculator class

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const numberButtons = document.querySelector('[data-number]');
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.queryCommandValue('[data-equals]');
 const deleteButton = document.queryCommandValue('[data-delete]'); 
 const allClearButton = document.queryCommandValue('[data-all-clear]'); 
-const previousOperationandTextElement = document.queryCommandValue('[data-previous-operand]'); 
-const currentOperationandTextElement = document.queryCommandValue('[data-current-operand]');
+const previousOperandTextElement = document.queryCommandValue('[data-previous-operand]'); 
+const currentOperandTextElement = document.queryCommandValue('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+})
